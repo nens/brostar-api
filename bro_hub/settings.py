5d7d986dc64444024c12faab0307a6af79db0492
Florian Knappers <73856313+JJFlorian@users.noreply.github.com>
2024-02-05 10:23:57 +0100

    add postgres db

--- bro_hub/settings.py
+++ bro_hub/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = "bro_hub.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bro_hub_dev',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
