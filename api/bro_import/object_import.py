1520d46fc3f3ff343444ce5f5ba9daa6217323fd
Florian Knappers <73856313+JJFlorian@users.noreply.github.com>
2024-02-06 12:00:03 +0100

    gmn importer completed

--- api/bro_import/object_import.py
+++ api/bro_import/object_import.py
@@ -1,5 +1,12 @@
+import requests
+import xmltodict
+import json
+
 from abc import ABC, abstractmethod
+from typing import IO, Dict, Any, List, Tuple
+
 from django.conf import settings
+from gmn.models import GMN, Measuringpoint
 
 class ObjectImporter(ABC):
     """Imports the BRO data based on the object ID.
@@ -18,35 +25,89 @@ class ObjectImporter(ABC):
     def __init__(self, bro_domain, bro_id):
         self.bro_domain = bro_domain
         self.bro_id = bro_id
-        print('test')
 
     def run(self):
         url = self._create_download_url()
-        # xml_file = self._download_xml(url)
-        # json_data = self._convert_xml_to_json(xml_file)
-
-        # self._save_data(json_data)
+        xml_data = self._download_xml(url)
+        json_data = self._convert_xml_to_json(xml_data)
+        self._save_data_to_database(json_data)
     
     def _create_download_url(self) -> str:
         """ Creates the import url for a given bro object.       
         """
         bro_domain = self.bro_domain.lower()
-        url = f"{settings.BRO_UITGIFTE_SERVICE_URL}/gm/{bro_domain}/v1/objects/{self.bro_id}?fullHistory=ja"
+        url = f"{settings.BRO_UITGIFTE_SERVICE_URL}/gm/{bro_domain}/v1/objects/{self.bro_id}?fullHistory=nee"
         return url
 
-    def _download_xml(self, url):
-        pass
+    def _download_xml(self, url: str) -> IO[bytes]:
+        """ Downloads the BRO XML file based on an url"""
+        r = requests.get(url=url)
+        r.raise_for_status()
 
-    def _convert_xml_to_json(self, xml_file):
-        pass
+        return r.content
+        
+
+    def _convert_xml_to_json(self, xml_data: IO[bytes]) -> Dict[str, Any]:
+        json_data = xmltodict.parse(xml_data)
+
+        return json_data
 
     @abstractmethod
-    def _save_downloaded_data(self):
+    def _save_data_to_database(self, json_data: Dict[str, Any]):
+        """ Saves the downloaded BRO data into the Django database."""
         pass
+
     
 class GMNObjectImporter(ObjectImporter):
-    def _save_downloaded_data(self):
-        pass
+    def _save_data_to_database(self, json_data: Dict[str, Any]):
+        gmn_data, measuringpoint_data = self._split_json_data(json_data)
+                    
+        gmn_object = self._save_gmn_data(gmn_data)
+        self._save_measuringpoint_data(measuringpoint_data)
+
+    def _split_json_data(self, json_data: Dict[str, Any]) -> Tuple[Dict[str, Any], List[Dict[str, Any]]]:
+        """ Takes in the json data and splits it up into GMN and Measuringpoint data"""
+        dispatch_document_data = json_data.get('dispatchDataResponse', {}).get('dispatchDocument', {})
+
+        measuringpoint_data = dispatch_document_data['GMN_PPO'].get('measuringPoint', [])
+
+        gmn_data = dispatch_document_data['GMN_PPO']
+        gmn_data.pop('measuringPoint', None)      
+
+        return gmn_data, measuringpoint_data
+    
+    def _save_gmn_data(self, gmn_data: Dict[str, Any]):
+        self.gmn_obj, created = GMN.objects.update_or_create(
+            bro_id=gmn_data.get('brocom:broId', None),
+            delivery_accountable_party=gmn_data.get('brocom:deliveryAccountableParty', None),
+            quality_regime=gmn_data.get('brocom:qualityRegime', None),
+            name=gmn_data.get('name', None),
+            delivery_context=gmn_data.get('deliveryContext', {}).get('#text', None),
+            monitoring_purpose=gmn_data.get('monitoringPurpose', {}).get('#text', None),
+            groundwater_aspect=gmn_data.get('groundwaterAspect', {}).get('#text', None),
+            start_date_monitoring=gmn_data.get('monitoringNetHistory', {}).get('startDateMonitoring', {}).get('brocom:date', None),
+            object_registration_time=gmn_data.get('registrationHistory', {}).get('brocom:objectRegistrationTime', None),
+            registration_status=gmn_data.get('registrationHistory', {}).get('brocom:registrationStatus', {}).get('#text', None),
+        )
+        
+        self.gmn_obj.save()
+
+
+    def _save_measuringpoint_data(self, measuringpoint_data):
+        for measuringpoint in measuringpoint_data:
+            mp_data = measuringpoint.get('MeasuringPoint', {})
+            monitoring_tube_data = mp_data.get('monitoringTube', {}).get('GroundwaterMonitoringTube', {})
+
+            measuringpoint_obj, created = Measuringpoint.objects.update_or_create(
+                gmn=self.gmn_obj,
+                measuringpoint_code = mp_data.get('measuringPointCode', None),
+                measuringpoint_start_date = mp_data.get('startDate', {}).get('brocom:date', None),
+                gmw_bro_id = monitoring_tube_data.get('broId', None),
+                tube_number = monitoring_tube_data.get('tubeNumber', None),
+                tube_start_date = monitoring_tube_data.get('startDate', None).get('brocom:date', None),
+            )
+
+            measuringpoint_obj.save()
 
 class GMWObjectImporter(ObjectImporter):
     pass
--- gmn/models.py
+++ gmn/models.py
@@ -4,13 +4,42 @@ from django.db import models
 
 
 class GMN(models.Model):
-    """The abbreviation GMN was intentionally chosen because it is the BRO term that is never used in full."""
+    """ Monitoring
+    
+    The abbreviation GMN was intentionally chosen, as it is the commonly used term in BRO land."""
 
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     bro_id = models.CharField(max_length=18)
+    delivery_accountable_party = models.CharField(max_length=8, blank=True, null=True)
+    quality_regime = models.CharField(max_length=50, blank=True, null=True)
+    name = models.CharField(max_length=100, blank=True, null=True)
+    delivery_context = models.CharField(max_length=100, blank=True, null=True)
+    monitoring_purpose = models.CharField(max_length=100, blank=True, null=True)
+    groundwater_aspect = models.CharField(max_length=100, blank=True, null=True)
+    start_date_monitoring = models.DateField(blank=True, null=True)
+    object_registration_time = models.DateTimeField(blank=True, null=True)
+    registration_status = models.CharField(max_length=50, blank=True, null=True)
 
     def __str__(self):
         return self.bro_id
 
     class Meta:
         verbose_name_plural = "GMN's"
+
+class Measuringpoint(models.Model):
+    """ A measuringpoint is a single object in a GMN.
+    
+    However, a measuring point is NOT a physical measuring point,
+    but rather an abstraction of it. It is linked to a physical GMW monitoringtube, 
+    which can be replaced.
+    """
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    gmn = models.ForeignKey(GMN, on_delete=models.SET_NULL, null=True, blank=True)
+    measuringpoint_code = models.CharField(max_length=50, blank=True, null=True)
+    measuringpoint_start_date = models.DateField(blank=True, null=True)
+    gmw_bro_id = models.CharField(max_length=50, blank=True, null=True)
+    tube_number = models.CharField(max_length=50, blank=True, null=True)
+    tube_start_date = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.measuringpoint_code
