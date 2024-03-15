f18dd3f5f9d8951e0e374046bd6306c5f016fbc1
Florian Knappers <73856313+JJFlorian@users.noreply.github.com>
2024-03-15 15:30:59 +0100

    Null=trues back

--- gmn/models.py
+++ gmn/models.py
@@ -16,15 +16,15 @@ class GMN(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     data_owner = models.ForeignKey(Organisation, on_delete=models.CASCADE)
     bro_id = models.CharField(max_length=18)
-    delivery_accountable_party = models.CharField(max_length=8)
-    quality_regime = models.CharField(max_length=50)
-    name = models.CharField(max_length=100)
-    delivery_context = models.CharField(max_length=100)
-    monitoring_purpose = models.CharField(max_length=100)
-    groundwater_aspect = models.CharField(max_length=100)
-    start_date_monitoring = models.DateField()
-    object_registration_time = models.DateTimeField()
-    registration_status = models.CharField(max_length=50)
+    delivery_accountable_party = models.CharField(max_length=8, null=True)
+    quality_regime = models.CharField(max_length=50, null=True)
+    name = models.CharField(max_length=100, null=True)
+    delivery_context = models.CharField(max_length=100, null=True)
+    monitoring_purpose = models.CharField(max_length=100, null=True)
+    groundwater_aspect = models.CharField(max_length=100, null=True)
+    start_date_monitoring = models.DateField(null=True)
+    object_registration_time = models.DateTimeField(null=True)
+    registration_status = models.CharField(max_length=50, null=True)
 
     def __str__(self):
         return self.bro_id
@@ -46,11 +46,11 @@ class Measuringpoint(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     data_owner = models.ForeignKey(Organisation, on_delete=models.CASCADE)
     gmn = models.ForeignKey(GMN, on_delete=models.CASCADE)
-    measuringpoint_code = models.CharField(max_length=50)
-    measuringpoint_start_date = models.DateField()
-    gmw_bro_id = models.CharField(max_length=50)
-    tube_number = models.CharField(max_length=50)
-    tube_start_date = models.DateField()
+    measuringpoint_code = models.CharField(max_length=50, null=True)
+    measuringpoint_start_date = models.DateField(null=True)
+    gmw_bro_id = models.CharField(max_length=50, null=True)
+    tube_number = models.CharField(max_length=50, null=True)
+    tube_start_date = models.DateField(null=True)
 
     def __str__(self):
         return self.measuringpoint_code
