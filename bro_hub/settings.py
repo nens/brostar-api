bf2178f613126e1f22a653ea42da15071a1dff8d
Florian Knappers <73856313+JJFlorian@users.noreply.github.com>
2024-03-18 16:38:00 +0100

    swagger/redoc

--- bro_hub/settings.py
+++ bro_hub/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "drf_yasg",
     "django_filters",
     "rest_framework_simplejwt",
     "encrypted_model_fields",
