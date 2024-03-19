8773ed29287dd5ea31b6966df58bbd04d3f1a19d
Florian Knappers <73856313+JJFlorian@users.noreply.github.com>
2024-03-19 16:58:51 +0100

    mixins added for required fields swagger

--- gmn/serializers.py
+++ gmn/serializers.py
@@ -1,17 +1,19 @@
 from rest_framework import serializers
 
-from api.mixins import UrlFieldMixin
+from api.mixins import RequiredFieldsMixin, UrlFieldMixin
 
 from . import models as gmn_models
 
 
-class GMNSerializer(UrlFieldMixin, serializers.ModelSerializer):
+class GMNSerializer(UrlFieldMixin, RequiredFieldsMixin, serializers.ModelSerializer):
     class Meta:
         model = gmn_models.GMN
         fields = "__all__"
 
 
-class MeasuringpointSerializer(UrlFieldMixin, serializers.ModelSerializer):
+class MeasuringpointSerializer(
+    UrlFieldMixin, RequiredFieldsMixin, serializers.ModelSerializer
+):
     class Meta:
         model = gmn_models.Measuringpoint
         fields = "__all__"
--- gmw/serializers.py
+++ gmw/serializers.py
@@ -1,17 +1,24 @@
 from rest_framework import serializers
 
-from api.mixins import UrlFieldMixin
+from api.mixins import RequiredFieldsMixin, UrlFieldMixin
 
 from . import models as gmw_models
 
 
-class GMWSerializer(UrlFieldMixin, serializers.ModelSerializer):
+class GMWSerializer(UrlFieldMixin, RequiredFieldsMixin, serializers.ModelSerializer):
     class Meta:
         model = gmw_models.GMW
         fields = "__all__"
 
 
-class MonitoringTubeSerializer(UrlFieldMixin, serializers.ModelSerializer):
+class MonitoringTubeSerializer(
+    UrlFieldMixin, RequiredFieldsMixin, serializers.ModelSerializer
+):
     class Meta:
         model = gmw_models.MonitoringTube
         fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.required = True
