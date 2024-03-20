9abaaa533a9ab1a15b4024ac0726d80d5894be3e
Florian Knappers <73856313+JJFlorian@users.noreply.github.com>
2024-03-20 10:37:40 +0100

    added extra info to measuringpoint endpoint

--- gmn/serializers.py
+++ gmn/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from api.mixins import RequiredFieldsMixin, UrlFieldMixin
+from gmw import models as gmw_models
 
 from . import models as gmn_models
 
@@ -14,6 +15,22 @@ class GMNSerializer(UrlFieldMixin, RequiredFieldsMixin, serializers.ModelSeriali
 class MeasuringpointSerializer(
     UrlFieldMixin, RequiredFieldsMixin, serializers.ModelSerializer
 ):
+    gmw_uuid = serializers.SerializerMethodField()
+    monitoringtube_uuid = serializers.SerializerMethodField()
+    location = serializers.SerializerMethodField()
+
     class Meta:
         model = gmn_models.Measuringpoint
         fields = "__all__"
+
+    def get_location(self, obj):
+        return gmw_models.GMW.objects.get(bro_id=obj.gmw_bro_id).standardized_location
+
+    def get_gmw_uuid(self, obj):
+        return gmw_models.GMW.objects.get(bro_id=obj.gmw_bro_id).uuid
+
+    def get_monitoringtube_uuid(self, obj):
+        gmw_uuid = self.get_gmw_uuid(obj)
+        return gmw_models.MonitoringTube.objects.get(
+            gmw=gmw_uuid, tube_number=obj.tube_number
+        ).uuid
