3ffcacf260cf4e51f6c25387d923ceaef7f9c715
Florian Knappers <73856313+JJFlorian@users.noreply.github.com>
2024-03-26 14:07:27 +0100

    increased pagination

--- brostar/settings.py
+++ brostar/settings.py
@@ -158,7 +158,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # DRF configuration
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 100,
+    "PAGE_SIZE": 1000,
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.BasicAuthentication",
