d5601ffc071afa7c793e416905a6de1e7c6246ec
Florian Knappers <73856313+JJFlorian@users.noreply.github.com>
2024-04-05 16:34:23 +0200

    Update authentication.py

--- brostar_api/authentication.py
+++ brostar_api/authentication.py
@@ -2,6 +2,7 @@ from rest_framework.authentication import SessionAuthentication
 
 
 class CustomSessionAuthentication(SessionAuthentication):
+    """Added to bypass the csrf requirement on POST requests"""
     def enforce_csrf(self, request):
         """The default SessionAuthentication class includes CSRF protection; this
         module contains a version that doesn't.
