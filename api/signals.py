21d8933ab018dcc48c2174215ae396f98d843752
Florian Knappers <73856313+JJFlorian@users.noreply.github.com>
2024-08-09 09:25:06 +0200

    attempt to fix signal on user porfile

--- api/signals.py
+++ api/signals.py
@@ -20,11 +20,21 @@ def create_user_profile(sender, instance: User, created, **kwargs):
     """After a user accepts a nens auth client invitation, this creates a UserProfile.
     The organisation initially set in the InviteUser is copied to the UserProfile.
     """
-    try:
-        print(instance.email)
-        invite_user = InviteUser.objects.get(email=instance.email)
-        UserProfile.objects.update_or_create(
-            user=instance, organisation=invite_user.organisation
-        )
-    except InviteUser.DoesNotExist:
-        UserProfile.objects.create(user=instance)
+    if created:
+        try:
+            UserProfile.objects.create(
+                user=instance,
+            )
+
+        except InviteUser.DoesNotExist:
+            UserProfile.objects.create(user=instance)
+
+    else:
+        try:
+            invite_user = InviteUser.objects.get(email=instance.email)
+            user_profile = UserProfile.objects.get(user=instance)
+            user_profile.organisation = invite_user.organisation
+            user_profile.save()
+
+        except InviteUser.DoesNotExist:
+            pass
