c927b830c232e1ffbe01cc3f3ed6087deb5f33d7
StevenHosper <steven.hosper@nelen-schuurmans.nl>
2025-01-28 08:15:18 +0100

    Correct ULR patterns and views

--- gld/urls.py
+++ gld/urls.py
@@ -3,14 +3,13 @@ from django.urls import path
 from . import views
 
 app_name = "gld"
-
 urlpatterns = [
     path("glds/", views.GLDListView.as_view(), name="gld-list"),
-    path("gld/<uuid:uuid>/", views.GLDDetailView.as_view(), name="gld-detail"),
-    path("observations/", views.GLDListView.as_view(), name="observation-list"),
+    path("glds/<uuid:uuid>/", views.GLDDetailView.as_view(), name="gld-detail"),
+    path("observations/", views.ObservationListView.as_view(), name="observation-list"),
     path(
         "observations/<uuid:uuid>/",
-        views.GLDDetailView.as_view(),
+        views.ObservationDetailView.as_view(),
         name="observation-detail",
     ),
 ]
