f6ece14ee9229163520521df01737e45fb05d740
StevenHosper <steven.hosper@nelen-schuurmans.nl>
2025-09-19 13:08:36 +0200

    fix mock_get

--- api/tests/test_tasks.py
+++ api/tests/test_tasks.py
@@ -69,7 +69,7 @@ def test_deliver_xml_file_task(
 
 @mock.patch("api.tasks.api_models.UploadTask.objects.get")
 @mock.patch("api.tasks.utils.check_delivery_status")
-def test_check_delivery_status_task_delivered(mock_sleep, mock_check_status, mock_get):
+def test_check_delivery_status_task_delivered(mock_check_status, mock_get):
     mock_instance = mock.Mock()
     mock_get.return_value = mock_instance
 
