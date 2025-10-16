b8c4d33dba640ac55e4cc383f7d01b64b9ce3a91
StevenHosper <stevenhosper@gmail.com>
2025-10-16 16:41:37 +0200

    Api filtering (#206)
    * date filtering for uploadtasks
    
    * temporarily, do not send to validate server

--- api/filters.py
+++ api/filters.py
@@ -4,6 +4,16 @@ from . import models as api_models
 
 
 class UploadTaskFilter(filters.FilterSet):
+    created__lt = filters.IsoDateTimeFilter(field_name="created", lookup_expr="lt")
+    created__lte = filters.IsoDateTimeFilter(field_name="created", lookup_expr="lte")
+    created__gt = filters.IsoDateTimeFilter(field_name="created", lookup_expr="gt")
+    created__gte = filters.IsoDateTimeFilter(field_name="created", lookup_expr="gte")
+
+    updated__lt = filters.IsoDateTimeFilter(field_name="updated", lookup_expr="lt")
+    updated__lte = filters.IsoDateTimeFilter(field_name="updated", lookup_expr="lte")
+    updated__gt = filters.IsoDateTimeFilter(field_name="updated", lookup_expr="gt")
+    updated__gte = filters.IsoDateTimeFilter(field_name="updated", lookup_expr="gte")
+
     class Meta:
         model = api_models.UploadTask
         exclude = ["metadata", "sourcedocument_data"]
