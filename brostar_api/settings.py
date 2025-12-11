830b42c6096c451e1b9444ddafded5772cda794f
StevenHosper <steven.hosper@ziggo.nl>
2025-12-11 12:28:50 +0100

    Add OAuth2 resourceserver and middelware

--- brostar_api/settings.py
+++ brostar_api/settings.py
@@ -16,6 +16,7 @@ SECRET_KEY = os.getenv(
 NENS_AUTH_ISSUER = os.getenv("NENS_AUTH_ISSUER")
 NENS_AUTH_CLIENT_ID = os.getenv("NENS_AUTH_CLIENT_ID")
 NENS_AUTH_CLIENT_SECRET = os.getenv("NENS_AUTH_CLIENT_SECRET")
+NENS_AUTH_RESOURCE_SERVER_ID = os.getenv("NENS_AUTH_RESOURCE_SERVER_ID")
 
 _debug_env = os.getenv("DEBUG", default="true")
 DATABASE_HOST = os.getenv("DATABASE_HOST", "db")
@@ -87,6 +88,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "nens_auth_client.middleware.AccessTokenMiddleware",
     "corsheaders.middleware.CorsMiddleware",
 ]
 
