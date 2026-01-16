69fe71ec652b011eeec45f6756fbb4941ba6a9a9
StevenHosper <steven.hosper@nelen-schuurmans.nl>
2026-01-16 16:33:19 +0100

    Easy GMW-Access and GMWConstructions

---
+++ gmw/xml_reader/namespaces.py
@@ -0,0 +1,31 @@
+# =============================================================================
+# GMW
+# =============================================================================
+ns_reg_gmw = {
+    "": "http://www.broservices.nl/xsd/dsgmw/1.1",
+    "brocom": "http://www.broservices.nl/xsd/brocommon/3.0",
+    "gmwcom": "http://www.broservices.nl/xsd/gmwcommon/1.1",
+    "gml": "http://www.opengis.net/gml/3.2",
+    "xsi": "http://www.w3.org/2001/XMLSchema-instance",
+}
+
+codespace_gmw = {
+    "deliveryContext": "urn:bro:gmw:DeliveryContext",
+    "constructionStandard": "urn:bro:gmw:ConstructionStandard",
+    "initialFunction": "urn:bro:gmw:InitialFunction",
+    "wellHeadProtector": "urn:bro:gmw:WellHeadProtector",
+    "horizontalPositioningMethod": "urn:bro:gmw:HorizontalPositioningMethod",
+    "groundLevelPositioningMethod": "urn:bro:gmw:GroundLevelPositioningMethod",
+    "tubeType": "urn:bro:gmw:TubeType",
+    "tubeStatus": "urn:bro:gmw:TubeStatus",
+    "tubeTopPositioningMethod": "urn:bro:gmw:TubeTopPositioningMethod",
+    "tubePackingMaterial": "urn:bro:gmw:TubePackingMaterial",
+    "tubeMaterial": "urn:bro:gmw:TubeMaterial",
+    "glue": "urn:bro:gmw:Glue",
+    "sockMaterial": "urn:bro:gmw:SockMaterial",
+    "electrodePackingMaterial": "urn:bro:gmw:ElectrodePackingMaterial",
+    "electrodeStatus": "urn:bro:gmw:ElectrodeStatus",
+    "localVerticalReferencePoint": "urn:bro:gmw:LocalVerticalReferencePoint",
+    "wellStability": "urn:bro:gmw:WellStability",
+    "correctionReason": "urn:bro:gmw:CorrectionReason",
+}
