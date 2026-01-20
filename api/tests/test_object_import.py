aa40a259709cd14e15a87e4e654e30a3c81cb728
StevenHosper <steven.hosper@nelen-schuurmans.nl>
2026-01-20 16:27:27 +0100

    temporarily comment integration tests

--- api/tests/test_object_import.py
+++ api/tests/test_object_import.py
@@ -9,7 +9,6 @@ from rest_framework.test import APIClient
 from api.bro_import import bulk_import, object_import
 from api.models import ImportTask, UploadTask
 from api.tests import fixtures
-from gld.models import GLD, Observation
 from gmw.models import GMW, Event, MonitoringTube
 
 user = fixtures.organisation_user
@@ -125,57 +124,58 @@ def test_convert_xml_to_json(gmn_object_importer):
     assert json_data == expected_json
 
 
-@pytest.fixture
-def gld_object_importer(organisation):
-    return object_import.GLDObjectImporter(
-        bro_id="GLD000000076615", data_owner=organisation
-    )
+### Temporarily no integration tests as ACC does not have this dossier.
+# @pytest.fixture
+# def gld_object_importer(organisation):
+#     return object_import.GLDObjectImporter(
+#         bro_id="GLD000000076615", data_owner=organisation
+#     )
 
 
-@pytest.mark.django_db
-def test_gld_download_url(gld_object_importer: object_import.GLDObjectImporter):
-    url = gld_object_importer._create_download_url()
+# @pytest.mark.django_db
+# def test_gld_download_url(gld_object_importer: object_import.GLDObjectImporter):
+#     url = gld_object_importer._create_download_url()
 
-    assert (
-        url
-        == f"{settings.BRO_UITGIFTE_SERVICE_URL}/gm/gld/v1/objects/GLD000000076615?fullHistory=nee&observationPeriodBeginDate=2021-01-01&observationPeriodEndDate=2021-01-01"
-    )
-
-
-@pytest.mark.django_db
-def test_gld_observation_summary(gld_object_importer: object_import.GLDObjectImporter):
-    observation_summary = gld_object_importer._observation_summary()
-
-    assert isinstance(observation_summary, list)
-
-    procedure = observation_summary[0]
-    assert isinstance(procedure, dict)
-
-    keys = procedure.keys()
-    assert "observationId" in keys
-    assert "startDate" in keys
-    assert "endDate" in keys
-    assert "observationType" in keys
-    assert "observationProcessId" in keys
-
-    assert procedure["startDate"] == "14-11-2024"
+#     assert (
+#         url
+#         == f"{settings.BRO_UITGIFTE_SERVICE_URL}/gm/gld/v1/objects/GLD000000076615?fullHistory=nee&observationPeriodBeginDate=2021-01-01&observationPeriodEndDate=2021-01-01"
+#     )
 
 
-@pytest.mark.django_db
-def test_gld_import(gld_object_importer: object_import.GLDObjectImporter):
-    gld_object_importer.run()
+# @pytest.mark.django_db
+# def test_gld_observation_summary(gld_object_importer: object_import.GLDObjectImporter):
+#     observation_summary = gld_object_importer._observation_summary()
 
-    gld_instance = GLD.objects.get(bro_id=gld_object_importer.bro_id)
-    assert gld_instance.quality_regime == "IMBRO/A"
-    assert gld_instance.gmw_bro_id == "GMW000000078271"
-    assert gld_instance.tube_number == "1"
+#     assert isinstance(observation_summary, list)
 
-    observation_instance = Observation.objects.filter(gld=gld_instance)
-    assert observation_instance.count() == gld_instance.nr_of_observations
-    assert observation_instance[0].end_position == datetime.date(2025, 1, 8)
-    assert observation_instance[0].begin_position == datetime.date(2024, 11, 14)
-    assert observation_instance[0].observation_type == "reguliereMeting"
-    assert observation_instance[0].validation_status == "voorlopig"
+#     procedure = observation_summary[0]
+#     assert isinstance(procedure, dict)
+
+#     keys = procedure.keys()
+#     assert "observationId" in keys
+#     assert "startDate" in keys
+#     assert "endDate" in keys
+#     assert "observationType" in keys
+#     assert "observationProcessId" in keys
+
+#     assert procedure["startDate"] == "14-11-2024"
+
+
+# @pytest.mark.django_db
+# def test_gld_import(gld_object_importer: object_import.GLDObjectImporter):
+#     gld_object_importer.run()
+
+#     gld_instance = GLD.objects.get(bro_id=gld_object_importer.bro_id)
+#     assert gld_instance.quality_regime == "IMBRO/A"
+#     assert gld_instance.gmw_bro_id == "GMW000000078271"
+#     assert gld_instance.tube_number == "1"
+
+#     observation_instance = Observation.objects.filter(gld=gld_instance)
+#     assert observation_instance.count() == gld_instance.nr_of_observations
+#     assert observation_instance[0].end_position == datetime.date(2025, 1, 8)
+#     assert observation_instance[0].begin_position == datetime.date(2024, 11, 14)
+#     assert observation_instance[0].observation_type == "reguliereMeting"
+#     assert observation_instance[0].validation_status == "voorlopig"
 
 
 @pytest.fixture
